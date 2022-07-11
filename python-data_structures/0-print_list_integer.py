c7a5331fa4648d7012f9bd70e8c9a506adbf25a9
gatwirikiriinya <f.kiriinya@alustudent.com>
2022-07-11 12:35:47 +0300

    Fix

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
+def print_list_integer(my_list):
     for x in range(len(my_list)):
-        print(*my_list, sep = "\n".format(x))
+        print("{:d}".format(my_list[x]))
+        
